c00c1c355d1174cc19d92991f0692c0094217afb
Dennis <delmore85@users.noreply.github.com>
2016-06-24 16:55:29 -0400

    Master Update
    Updating files to be up-to-date with task plus having the datepickers colored.

--- calendar.js
+++ calendar.js
@@ -1,11 +1,27 @@
-﻿function myRequest() {
+﻿$(function(){
+    $('#startDateSelect').datepicker({ changeYear: true, changeMonth: true });
+});
+
+function myRequest() {
   var request = new XMLHttpRequest();
-  var uri = "http://holidayapi.com/v1/holidays?country=US&year=2016&month=06";
+  var dateRequest = encodeURIComponent(document.getElementById("startDateSelect").value);
+  var calDate = document.getElementById("startDateSelect").value;
+  var daysRequest = encodeURIComponent(document.getElementById("daysSelect").value);
+  var countryRequest = encodeURIComponent(document.getElementById("countrySelect").value);
+  var url = "http://holidayapi.com/v1/holidays?country="+countryRequest+"&year=2016&month=06";
   request.onreadystatechange = function() {
     if (request.readyState == 4 && request.status == 200) {
       document.getElementById("cal").innerHTML = request.responseText;
     }
   };
-  request.open("GET", uri , true);
+  request.open("GET", url , true);
   request.send();
+  $('#myCal').datepicker({
+		inline: true,
+		firstDay: 0,
+		showOtherMonths: false,
+		dayNamesShort: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+	});
+  $('#myCal').datepicker('setDate', calDate);
 }
+
