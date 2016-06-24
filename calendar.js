1f8e203e0f0bfffde3292c1451a8f7c838952326
Dennis <delmore85@users.noreply.github.com>
2016-06-24 14:36:53 -0400

    Country Code Update
    Updated Code to accept the country code from the drop down and add it into the request to holidayapi to properly display the holidays in selected country

--- calendar.js
+++ calendar.js
@@ -1,12 +1,15 @@
 ﻿function myRequest() {
   var request = new XMLHttpRequest();
-  var uri = "http://holidayapi.com/v1/holidays?country=US&year=2016&month=06";
+  var dateRequest = encodeURIComponent(document.getElementById("startDateSelect").value)
+  var daysRequest = encodeURIComponent(document.getElementById("daysSelect").value)
+  var countryRequest = encodeURIComponent(document.getElementById("countrySelect").value)
+  var url = "http://holidayapi.com/v1/holidays?country="+countryRequest+"&year=2016&month=06";
   request.onreadystatechange = function() {
     if (request.readyState == 4 && request.status == 200) {
       document.getElementById("cal").innerHTML = request.responseText;
     }
   };
-  request.open("GET", uri , true);
+  request.open("GET", url , true);
   request.send();
 }
 
