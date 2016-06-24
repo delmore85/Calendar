748db7490c074eae928160e086f5f8e4a7b29df1
Dennis <delmore85@users.noreply.github.com>
2016-06-24 13:22:06 -0400

    Uploading Initial Files
    Initial files include:
    calendar.html - Has the base form for the getting the user input needed for holidayapi. Also has a div dedicated to where the actual calendar will ultimately go.
    calendar.css - Quick minimal css for the form
    calendar.js - A basic GET request to retrieve base info for the this month from holidayapi which is displayed as text on calendar.html in the div reserved for the calendar.

---
+++ calendar.js
@@ -0,0 +1,11 @@
+﻿function myRequest() {
+  var request = new XMLHttpRequest();
+  var uri = "http://holidayapi.com/v1/holidays?country=US&year=2016&month=06";
+  request.onreadystatechange = function() {
+    if (request.readyState == 4 && request.status == 200) {
+      document.getElementById("cal").innerHTML = request.responseText;
+    }
+  };
+  request.open("GET", uri , true);
+  request.send();
+}
