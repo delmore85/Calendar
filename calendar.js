3bec77540e55fbb0ed9a922f904a85cd03af6abd
Dennis <delmore85@users.noreply.github.com>
2016-06-24 15:32:34 -0400

    Calendar Connect
    Connected calendar to date input. Ran into issues getting the calendar to jump to correct date and realized I was using the "encoded" version. Created a separate variable to hold just the value entered. Tried using date field instead of datepicker but had issues. Will try to revisit this and try to get input from date field

--- calendar.js
+++ calendar.js
@@ -4,9 +4,10 @@
 
 function myRequest() {
   var request = new XMLHttpRequest();
-  var dateRequest = encodeURIComponent(document.getElementById("startDateSelect").value)
-  var daysRequest = encodeURIComponent(document.getElementById("daysSelect").value)
-  var countryRequest = encodeURIComponent(document.getElementById("countrySelect").value)
+  var dateRequest = encodeURIComponent(document.getElementById("startDateSelect").value);
+  var calDate = document.getElementById("startDateSelect").value;
+  var daysRequest = encodeURIComponent(document.getElementById("daysSelect").value);
+  var countryRequest = encodeURIComponent(document.getElementById("countrySelect").value);
   var url = "http://holidayapi.com/v1/holidays?country="+countryRequest+"&year=2016&month=06";
   request.onreadystatechange = function() {
     if (request.readyState == 4 && request.status == 200) {
@@ -15,12 +16,12 @@ function myRequest() {
   };
   request.open("GET", url , true);
   request.send();
-    
   $('#myCal').datepicker({
 		inline: true,
 		firstDay: 0,
 		showOtherMonths: false,
 		dayNamesShort: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
 	});
+  $('#myCal').datepicker('setDate', calDate);
 }
 
