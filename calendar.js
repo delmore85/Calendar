c5032829fbbdc9dfb2c710cd5e88569f14f7e595
Dennis <delmore85@users.noreply.github.com>
2016-06-26 18:48:00 -0400

    Color Correction and Date Function Update
    Corrected colors to only affect the display calendar. Tied the date selection input to the holidayapi request which now properly displays the selected date in the test text.

--- calendar.js
+++ calendar.js
@@ -8,20 +8,27 @@ function myRequest() {
   var calDate = document.getElementById("startDateSelect").value;
   var daysRequest = encodeURIComponent(document.getElementById("daysSelect").value);
   var countryRequest = encodeURIComponent(document.getElementById("countrySelect").value);
-  var url = "http://holidayapi.com/v1/holidays?country="+countryRequest+"&year=2016&month=06";
+  var dateSplit = calDate.split('/');
+  var monthSplit = dateSplit[0];
+  var yearSplit = dateSplit[2];
+  var url = "http://holidayapi.com/v1/holidays?country="+countryRequest+"&year="+yearSplit+"&month="+monthSplit;
+  
   request.onreadystatechange = function() {
     if (request.readyState == 4 && request.status == 200) {
-      document.getElementById("cal").innerHTML = request.responseText;
+      document.getElementById("cal").innerHTML = request.responseText + "<br />" + calDate + "<br />" + monthSplit + "<br />" + yearSplit;
     }
   };
+  
   request.open("GET", url , true);
   request.send();
+  
   $('#myCal').datepicker({
 		inline: true,
 		firstDay: 0,
 		showOtherMonths: false,
 		dayNamesShort: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
 	});
+	
   $('#myCal').datepicker('setDate', calDate);
 }
 
